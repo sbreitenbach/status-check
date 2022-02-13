3c576fafdaaa6dc10b96cb1afcd80ef5c3ea9170
Scott Breitenbach <scott.breitenbach@gmail.com>
2022-02-13 17:18:24 -0500

    Running the OCR check

--- main.py
+++ main.py
@@ -94,6 +94,24 @@ def extract_text_from_image(image_path):
     result = result.strip()
     return result
 
+
+def check_images_for_error_words(dir_path, error_words):
+    error_word_pass = True
+    for img_path in os.listdir(dir_path):
+        img_path = os.path.join(dir_path, img_path)
+        if os.path.isfile(img_path):
+            result = extract_text_from_image(img_path)
+            if any(error_word in result for error_word in error_words):
+                logging.error(f"{img_path} contains error word")
+                print(f"{img_path} contains error word")
+                error_word_pass = False
+            else:
+                logging.debug(f"{img_path} does not contain error word")
+        else:
+            logging.warning(f"{img_path} does not exist")
+    return error_word_pass
+
+
 if __name__ == '__main__':
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -119,7 +137,9 @@ if __name__ == '__main__':
     image_status = compare_images_in_dirs(
         most_recent_dir_path, second_most_recent_dir_path)
 
-    if http_status & image_status:
+    error_word_status = check_images_for_error_words(most_recent_dir_path, error_words)
+
+    if http_status & image_status & error_word_status:
         print("All sites passed the status check")
     else:
         print("One or more sites failed the status check")
--- test_main.py
+++ test_main.py
@@ -9,7 +9,7 @@ def test_plways_passes():
 GSD1 = "test_resources/gsd1.jpeg"
 GSD2 = "test_resources/gsd2.jpeg"
 GSD3 = "test_resources/gsd3.jpeg"
-TEXT_IMAGE = "test_resources/text.png"
+TEXT_IMAGE = "test_resources/text.jpeg"
 
 def test_image_compare_same():
     assert(main.compare_images(GSD1, GSD1) == 1.0)
@@ -22,3 +22,12 @@ def test_image_compare_diff_2():
 
 def test_ocr_text():
     assert(main.extract_text_from_image(TEXT_IMAGE) == "test image")
+
+def test_error_words_failing_full_word():
+    assert(main.check_images_for_error_words("test_resources/", ["test"]) == False)
+
+def test_error_words_failing_partial_word():
+    assert(main.check_images_for_error_words("test_resources/", ["tes"]) == False)
+
+def test_error_words_passing():
+    assert(main.check_images_for_error_words("test_resources/", ["foo"]) == True)
